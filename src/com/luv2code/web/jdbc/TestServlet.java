34097076542f6b98307fd54aabc277b69bdcbaa1
chrisbrinkmann <brinkmcd@gmail.com>
2019-11-07 16:02:47 -0500

    outline TestServlet doGet() functionality

--- src/com/luv2code/web/jdbc/TestServlet.java
+++ src/com/luv2code/web/jdbc/TestServlet.java
@@ -25,8 +25,15 @@ public class TestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// set up the printwriter
+		
+		// get a connection to the database
+		
+		// create sql statements
+		
+		// execute sql statements
+		
+		// process the result set
 	}
 
 }
